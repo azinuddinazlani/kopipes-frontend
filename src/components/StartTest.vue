ba1308fc8df208c98e1c92505052df55a4cb2b3b
SanicLee69 <saniclee0@gmail.com>
2025-01-23 11:53:03 +0800

    init start test component

---
+++ src/components/StartTest.vue
@@ -0,0 +1,15 @@
+<!-- azin component -->
+<template>
+  <div class="wrapper">
+    <div></div>
+  </div>
+</template>
+
+<script setup>
+//
+</script>
+
+<style lang="scss" scoped>
+.wrapper {
+}
+</style>
