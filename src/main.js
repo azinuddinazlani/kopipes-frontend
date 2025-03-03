b15b7c73b7fb6ceae3dda04c0292ced13d07ee18
SanicLee69 <saniclee0@gmail.com>
2025-03-03 13:39:13 +0800

    init pinia, change tab name, delete garbage

--- src/main.js
+++ src/main.js
@@ -2,16 +2,18 @@ import './assets/index.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 
 import App from './App.vue'
 import router from './router'
-
 import Antd from 'ant-design-vue'
 // import 'ant-design-vue/dist/antd.css'
 
 const app = createApp(App)
+const pinia = createPinia()
+pinia.use(piniaPluginPersistedstate)
 
-app.use(createPinia())
+app.use(pinia)
 app.use(router)
 // ant design vue
 app.use(Antd)
--- src/views/ComponentView.vue
+++ src/views/ComponentView.vue
@@ -4,23 +4,19 @@
     <!-- <AssessmentProgress /> -->
     <!-- <Question /> -->
     <!-- <UserProfile /> -->
-    <!-- <div class="upload-wrapper">
-      <UploadResume class="uploadResume" />
-    </div> -->
     <!-- <PersonalDetailForm /> -->
-    <AssessmentResultPage />
+    <!-- <AssessmentResultPage /> -->
     <!-- <UserTypeSelection /> -->
   </div>
 </template>
 
 <script setup lang="ts">
 // import UserTypeSelection from '@/components/UserTypeSelection.vue';
-import AssessmentResultPage from '@/components/AssessmentResultPage.vue'
+// import AssessmentResultPage from '@/components/AssessmentResultPage.vue'
 // import StartTest from '@/components/StartTest.vue'
 // import AssessmentProgress from '@/components/AssessmentProgress.vue'
 // import Question from '@/components/Question.vue'
 // import UserProfile from '@/components/UserProfile.vue'
-// import UploadResume from '@/components/UploadResume.vue'
 // import PersonalDetailForm from '@/components/PersonalDetailForm.vue'
 </script>
 
